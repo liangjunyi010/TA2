74e692bebd9e58437dac7b22515231813fa23252
liangjunyi010 <liangjunyi010@gmail.com>
2025-03-19 14:13:44 +0800

    [update]:test.py

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import pickle
 
-with open("./Output/Adversarial/toxigen_adv.pkl", "rb") as f:
+with open("Output/Adversarial/toxigen_adv_1.pkl", "rb") as f:
     data = pickle.load(f)
 
-# 逐条打印，便于阅读
 for i, text in enumerate(data, start=1):
     print(f"=== Entry {i} ===")
-    # 如果你想把其中 \n 原样换行，可以再做一次替换
-    # text = text.replace("\\n", "\n")
     print(text)
     print()
